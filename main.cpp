37115fbe82514de4f319155c74c66f9f93937461
niccolonardelli <niccolo.nardelli@marconibari.edu.it>
2024-11-13 18:05:05 +0000

    consegna

--- main.cpp
+++ main.cpp
@@ -1,11 +1,53 @@
 #include <iostream>
 using namespace std;
+int main()
+{
+    int blocchi, altezza, piano;
+    cout << "quanti blocchi hai a disposizione?" << endl;
+    altezza = 0;
+    cin>>blocchi;
+    if (blocchi >=1)
+    {
+        altezza = altezza + 1;
+        if (blocchi >=3)
+        {
+           
+            altezza = altezza + 1;            
+            if (blocchi>=6)
+            {
+                altezza = altezza + 1;
+                blocchi = blocchi - 6;
+            }
+            else
+            {
+                cout << " altezza = " << altezza << endl;
+            }
+        }
+        else
+        {
+            cout << " altezza = " << altezza << endl;
+        }
 
-int main() {
-    string nome;
-    cout << "Inserisci il tuo nome: ";
-    cin >> nome;
-    cout << "Ciao " <<nome<< "!" << endl;
+    }
+    else 
+    {
+
+    }
+    piano = 3;
+    while (blocchi >=2)
+    {
+        if (blocchi >= piano + 2)
+        {
+            blocchi = blocchi -piano-2;
+            altezza = altezza + 1;
+            piano = piano + 2;
+        }
+        else
+        {
+            blocchi = 0;
+        }
+    }
+    cout << " altezza = " << altezza << endl;
 }
 
 //LEGGERE LE ISTRUZIONI NEL FILE README.md
